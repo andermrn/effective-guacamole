eed1103e066139a42217bc99e498b7eb67f088e9
Anderson Moreno <andermrn2004@gmail.com>
2026-03-11 00:30:36 -0300

    Add files via upload
    Exercício básico de somar dois números.

---
+++ soma-dois-numeros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n1, n2, s;
+    //Passo 01
+    cout << "INICIO\n";
+    cout << "Digite um valor: ";
+    cin >> n1;
+    //Passo 02
+    cout << "Digite outro valor: ";
+    cin >> n2;
+    //Passo 03
+    s = (n1+n2);
+    cout << "O resultado e: " << s << endl;
+    cout << "FIM.";
+}
